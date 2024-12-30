8b44b5c3a445a25aad23c3ec23305cdf6ad0353f
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2024-12-30 18:41:46 +0300

    Some touchups

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
+app.use(express.static(path.join(__dirname + '/public')));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var date = moment();
-
-app.use(express.static(path.join(__dirname + '/public')));
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
