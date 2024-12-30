42543bfc514d9c59a362317991753e66fb7b1b09
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2024-12-30 19:09:47 +0300

    version 1.01

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.use(express.static(path.join(__dirname + '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const app = express();
-const port = 3000;
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var date = moment();
 
