6b5b09477f8312af0f0b0541d6ff9f6132ea3f10
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-01-09 23:55:05 +0300

    Version 1.15, visual overhaul, add post delete functionality

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
+// Creating the express server
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Using middleware
 app.use(express.static(path.join(__dirname + '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var date = moment();
-
-let posts = [];
 let id = 0;
 
 app.get("/", (req, res) => {
@@ -32,13 +32,20 @@ app.post("/", (req, res) => {
               topic: req.body.topic,
               subject: req.body.subject,
               post: req.body.post});
-  console.log(posts);
   res.render("index.ejs",{posts: posts});
 });
 
-app.get("/posts.ejs", (req, res) => {
+app.get("/posts", (req, res) => {
   res.render("posts.ejs",{posts: posts});
 });
+
+app.get("/posts/delete/:id", (req, res)=>{
+  posts = posts.filter((post) => post.id !== parseInt(req.params.id));
+  console.log(res.status(200));
+  res.redirect("/posts");
+});
+
+let posts = [];
 
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
