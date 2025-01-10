4d071ccd64f42bc8e979e61615e5c4ef8618b2c9
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-01-10 19:08:51 +0300

    Version 1.2, add edit functionality, minor visual overhaul

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 
-// Creating the express server
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Using middleware
 app.use(express.static(path.join(__dirname + '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var date = moment();
-let id = 0;
+let id = 1;
 
-app.get("/", (req, res) => {
+// GET main page
+app.get("/", (req, res)=>{
   res.render("index.ejs", {posts: posts});
 });
 
-app.post("/", (req, res) => {
+// POST a new post
+app.post("/", (req, res)=>{
   var postDate = date.format('YYYY/MM/DD HH:mm:ss');
   id++;
   posts.push({id: id,
@@ -32,21 +32,51 @@ app.post("/", (req, res) => {
               topic: req.body.topic,
               subject: req.body.subject,
               post: req.body.post});
-  res.render("index.ejs",{posts: posts});
+  res.redirect("/");
 });
 
-app.get("/posts", (req, res) => {
+// GET the posts page
+app.get("/posts", (req, res)=>{
   res.render("posts.ejs",{posts: posts});
 });
 
+// DELETE a post based on its ID
 app.get("/posts/delete/:id", (req, res)=>{
   posts = posts.filter((post) => post.id !== parseInt(req.params.id));
-  console.log(res.status(200));
+  console.log("Delete ok! Status 200");
   res.redirect("/posts");
 });
 
-let posts = [];
+app.get("/posts/edit/:id", (req, res)=>{
+  var postToUpdate = posts.find((post) => post.id === parseInt(req.params.id));
+  res.render("edit.ejs", {post: postToUpdate});
+});
 
-app.listen(port, () => {
+app.post("/posts/edit/:id", (req, res)=>{
+  var postToUpdate = posts.find((post) => post.id === parseInt(req.params.id));
+  if (!postToUpdate) console.log("Update OK!");
+
+  if (req.body.username) postToUpdate.username = req.body.username;
+  if (req.body.subject) postToUpdate.subject = req.body.subject;
+  if (req.body.topic) postToUpdate.topic = req.body.topic;
+  if (req.body.post) postToUpdate.post = req.body.post;
+  postToUpdate.date = date.format('YYYY/MM/DD HH:mm:ss');
+  res.redirect("/posts");
+});
+
+// List of posts
+let posts = [
+  {
+    id: 1,
+    username: "Waleed",
+    subject: "Lorem Ipsum",
+    topic: "Linguistics",
+    post: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    date: date.format('YYYY/MM/DD HH:mm:ss')
+  }
+];
+
+// Connecting the server to the port
+app.listen(port, ()=>{
   console.log(`Server listening to port ${port}`);
 });
